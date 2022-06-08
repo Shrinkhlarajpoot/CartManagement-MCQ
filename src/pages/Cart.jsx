0bf270891a7cc065720ee9cdd815e5956b92cf3f
Shrinkhla Rajpoot <shrinkhlarajpoot@gmail.com>
2022-06-08 22:49:41 +0530

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,29 +1,28 @@
-import { Navigate, useNavigate } from "react-router-dom";
 import { CartReceipt } from "../components/cart-receipt";
 import { Cartcard } from "../components/cartcard/cartcard";
-import { Productcard } from "../components/productcard/productcard";
-import { useCart } from "../context/cart-context"
-import { products } from "../data";
 
-export const Cart = ()=>{
+import { useCart } from "../context/cart-context";
 
- const {cartState:{cartProducts}}= useCart();
+export const Cart = () => {
+  const {
+    cartState: { cartProducts },
+  } = useCart();
 
-    return(
-       
-        
-    
-      <div> {cartProducts.length>0 ?
-       <div className="cart_container">
-        <div className="product_wrapper">
-            {cartProducts?.map((product)=>
-            <Cartcard product={product} key={product.id}/>)}
+  return (
+    <div>
+      {" "}
+      {cartProducts.length > 0 ? (
+        <div className="cart_container">
+          <div className="product_wrapper">
+            {cartProducts?.map((product) => (
+              <Cartcard product={product} key={product.id} />
+            ))}
+          </div>
+          <div> {cartProducts.length > 0 && <CartReceipt />}</div>
         </div>
-         <div> {cartProducts.length>0 && <CartReceipt/>}</div></div>
-        
-        :<h1>No Products Added</h1>}</div>
-     
-    
-      
-    )
-}
+      ) : (
+        <h1>No Products Added</h1>
+      )}
+    </div>
+  );
+};
